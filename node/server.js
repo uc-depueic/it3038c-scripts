76073e97a57ce00b6c1696c78e50938bbc1c8afe
uc-depueic <depueic@mail.uc.edu>
2021-10-06 17:27:54 -0400

    lab6

--- node/server.js
+++ node/server.js
@@ -1,12 +1,17 @@
 var http = require("http");
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type": "text/text"});
+http.createServer(function(req, res){
 
-    res.end("Hello from Node!") 
+    if (req.url === "/") {
+        fs.readFile("./node/index.html", "UTF-8", function(err, body){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(body);
+    });
+}
+    else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end(`404 File Not Found at ${req.url}`);
+    }
+}).listen(3000);
 
-})
-
-server.listen(3000)
-
-console.log("Server listening on port 3000")
+console.log("Server listening on port 3000"); 
