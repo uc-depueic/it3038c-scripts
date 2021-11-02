9265f67e8a631561cc89cc39741a730687c40c9d
uc-depueic <depueic@mail.uc.edu>
2021-11-02 18:14:40 -0400

    lab10

--- python/lab8scrape.py
+++ python/lab8scrape.py
@@ -1,8 +1,8 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-data = requests.get("https://www.uefa.com/uefachampionsleague/statistics/players/").content 
+data = requests.get("https://www.walmart.com/cp/food/976759").content 
 soup = BeautifulSoup(data, 'html.parser')
-##details = soup.find("div class", {"primary":"()"})
-details = soup.findAll("Player")
+details = soup.find("h2",  {"class": "w_Cv lh-title ma0"})
+##details = soup.find("Seasonal flavors")
 print(details) 
---
+++ python/weatherapi.py
@@ -0,0 +1,5 @@
+import json
+
+import requests
+
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=47025,us&appid=2d1522c8d08e3fbb3d9ecb24df63f402')
