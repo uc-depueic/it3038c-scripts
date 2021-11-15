b9e8d31af6f5c9f93086b39419c707011aba9643
uc-depueic <depueic@mail.uc.edu>
2021-11-14 19:14:15 -0500

    lab8

--- python/lab8scrape.py
+++ python/lab8scrape.py
@@ -1,14 +1,14 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-r = requests.get('https://www.walmart.com/cp/food/976759').content
+r = requests.get('http://books.toscrape.com').content
 
 ##data = requests.get("https://www.walmart.com/cp/food/976759") 
 soup = BeautifulSoup(r, 'html.parser')
 
 
 ##details = soup.findALL("h2",  {"class": "w_wCv lh-title ma0"})
-details = soup.findAll(class_="Seasonal Flavors")
+details = soup.findAll(re.compile('h[1-6]'))
 print(details) 
 
 ##print(soup.prettify())
